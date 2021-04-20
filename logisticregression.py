81e0bce66fef0d7c4f0f7698c9eaf060b6ef963f
prasad <prasad.athave@iitgn.ac.in>
2021-04-20 22:37:57 +0530

    Q2 done almost

--- logisticregression.py
+++ logisticregression.py
@@ -39,14 +39,35 @@ class logisticregression:
 
     def fit(self,X,y,intercept_addition=True,n_iterations = 100,learning_rate = 0.01,use_autograd=False):
 
-        def cost_function(weights):
+        def cost_function_normal(weights):
             # weights = np.array(weights)
             ab = anp.dot(X,weights)
             ab = anp.array(ab)
             ab = sigmoid_function1(ab)
             cost = np.sum(np.abs(y-ab)**2)
             return cost
-            
+        def cost_function_L1(weights,lmbda=0.1):
+            # weights = np.array(weights)
+        
+            ab = anp.dot(X,weights)
+            ab = anp.array(ab)
+            ab = sigmoid_function1(ab)
+            cost = np.sum(np.abs(y-ab)**2)
+            cost += lmbda*(np.sum(np.abs(weights)))
+            return cost
+        
+        def cost_function_L2(weights,lmbda=0.1):
+            # weights = np.array(weights)
+        
+            ab = anp.dot(X,weights)
+            ab = anp.array(ab)
+            ab = sigmoid_function1(ab)
+            cost = np.sum(np.abs(y-ab)**2)
+            cost += lmbda*anp.dot(weights.T,weights)
+            return cost
+        
+
+
         if(intercept_addition==True):
             self.intercept_addition=True
             column = np.ones((X.shape[0],1))
@@ -68,14 +89,7 @@ class logisticregression:
                     gradient_displacement = np.dot(X.T,error_val)
                     self.weights += learning_rate*gradient_displacement
                 else:
-                    def cost_function(weights):
-                        # weights = np.array(weights)
-                        ab = anp.dot(X,weights)
-                        ab = anp.array(ab)
-                        ab = sigmoid_function1(ab)
-                        cost = np.sum(np.abs(y-ab)**2)
-                        return cost
-                    grad_cost = elementwise_grad(cost_function)
+                    grad_cost = elementwise_grad(cost_function_normal)
                     gradient_displacement = grad_cost(anp.array(self.weights))
                     gradient_displacement = np.array(gradient_displacement)
                     self.weights += -learning_rate*gradient_displacement
@@ -90,13 +104,26 @@ class logisticregression:
                 error_val = (y-predicted_vals)
                 
 
-                grad_cost = elementwise_grad(cost_function)
-                gradient_displacement = grad_cost(anp.array(self.weights))
+                grad_cost_L1 = elementwise_grad(cost_function_L1)
+                gradient_displacement = grad_cost_L1(anp.array(self.weights))
                 gradient_displacement = np.array(gradient_displacement)
                 self.weights += -learning_rate*gradient_displacement
 
         elif(self.regularization=="L2"):
-            pass
+            self.weights = np.ones(X.shape[1])
+            for i in range(n_iterations):
+                ### found the predictions for a step 
+                values = np.dot(X,self.weights)
+                # print(values)
+                
+                predicted_vals = sigmoid_function(values)
+                error_val = (y-predicted_vals)
+                
+                grad_cost_L2 = elementwise_grad(cost_function_L2)
+                gradient_displacement = grad_cost_L2(anp.array(self.weights))
+                gradient_displacement = np.array(gradient_displacement)
+                self.weights += -learning_rate*gradient_displacement
+            
 
 
     
@@ -132,7 +159,7 @@ kf = KFold(n_splits=3)
 acc =0
 for train, test in kf.split(X):
     X_train,y_train,X_test,y_test = X[train],y[train],X[test],y[test]
-    lr = logisticregression(regularization="No")
+    lr = logisticregression(regularization="L2")
     lr.fit(X_train,y_train,intercept_addition=True,n_iterations=100,learning_rate=0.01,use_autograd=True)
     prediction = lr.predict(X_test)
     acc += ((prediction==y_test).sum()/(y_test.shape[0]))*100
