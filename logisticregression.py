cf674e5cfdf4788f8fbad1a132e8540c36e2439f
prasad <prasad.athave@iitgn.ac.in>
2021-04-20 22:14:10 +0530

    some change

--- logisticregression.py
+++ logisticregression.py
@@ -2,13 +2,12 @@ import math
 import numpy as np
 import autograd.numpy
 import matplotlib.pyplot as plt
-from scipy.optimize import minimize
 from sklearn.datasets import load_breast_cancer
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
 from autograd import elementwise_grad
 import autograd.numpy as anp
+from sklearn.model_selection import KFold
 
 def sigmoid_one_value(value):
     # print(value.shape)
@@ -39,12 +38,20 @@ class logisticregression:
         self.weights = []
 
     def fit(self,X,y,intercept_addition=True,n_iterations = 100,learning_rate = 0.01,use_autograd=False):
-        print(X.shape)
-        print(y.shape)
+
+        def cost_function(weights):
+            # weights = np.array(weights)
+            ab = anp.dot(X,weights)
+            ab = anp.array(ab)
+            ab = sigmoid_function1(ab)
+            cost = np.sum(np.abs(y-ab)**2)
+            return cost
+            
         if(intercept_addition==True):
             self.intercept_addition=True
             column = np.ones((X.shape[0],1))
             X = np.hstack((column,X))
+
         if(self.regularization=="No"):            
             self.weights = np.ones(X.shape[1])
             for i in range(n_iterations):
@@ -72,19 +79,38 @@ class logisticregression:
                     gradient_displacement = grad_cost(anp.array(self.weights))
                     gradient_displacement = np.array(gradient_displacement)
                     self.weights += -learning_rate*gradient_displacement
+        elif(self.regularization=="L1"):
+            self.weights = np.ones(X.shape[1])
+            for i in range(n_iterations):
+                ### found the predictions for a step 
+                values = np.dot(X,self.weights)
+                # print(values)
+                
+                predicted_vals = sigmoid_function(values)
+                error_val = (y-predicted_vals)
+                
+
+                grad_cost = elementwise_grad(cost_function)
+                gradient_displacement = grad_cost(anp.array(self.weights))
+                gradient_displacement = np.array(gradient_displacement)
+                self.weights += -learning_rate*gradient_displacement
+
+        elif(self.regularization=="L2"):
+            pass
+
 
     
     def predict(self,X):
         if(self.intercept_addition==True):
             column = np.ones((X.shape[0],1))
             X = np.hstack((column,X))
-        print("prediction started")
-        print(self.weights.shape)
-        print(X.shape)
+        # print("prediction started")
+        # print(self.weights.shape)
+        # print(X.shape)
         a = np.dot(X,self.weights)
         a[a>=0.5] = 1
         a[a<0.5] = 0
-        print(a.shape)
+        # print(a.shape)
         return a
 
 
@@ -93,10 +119,25 @@ X,y = load_breast_cancer(return_X_y=True)
 sc = StandardScaler().fit(X)
 X = sc.transform(X)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, stratify=y, random_state=0)
-lr = logisticregression(regularization="No")
-lr.fit(X_train,y_train,intercept_addition=True,n_iterations=1000,learning_rate=0.01,use_autograd=False)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, stratify=y, random_state=0)
+# lr = logisticregression(regularization="No")
+# lr.fit(X_train,y_train,intercept_addition=True,n_iterations=1000,learning_rate=0.01,use_autograd=False)
 
-prediction = lr.predict(X_test)
-print(y_train.shape)
-print((prediction==y_test).sum())
+# prediction = lr.predict(X_test)
+
+# print(y_train.shape)
+# print((prediction==y_test).sum())
+
+kf = KFold(n_splits=3)
+acc =0
+for train, test in kf.split(X):
+    X_train,y_train,X_test,y_test = X[train],y[train],X[test],y[test]
+    lr = logisticregression(regularization="No")
+    lr.fit(X_train,y_train,intercept_addition=True,n_iterations=100,learning_rate=0.01,use_autograd=True)
+    prediction = lr.predict(X_test)
+    acc += ((prediction==y_test).sum()/(y_test.shape[0]))*100
+    
+print(acc/3)
+
+# without autograd = 95.95841455490579
+# with autograd = 97.54107490949598
