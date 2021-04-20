02ba5f575cb3c012359018f7a608a3fd69219ce9
prasad <prasad.athave@iitgn.ac.in>
2021-04-20 18:17:55 +0530

    some changes

--- logisticregression.py
+++ logisticregression.py
@@ -1,5 +1,11 @@
 import math
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import minimize
+from sklearn.datasets import load_breast_cancer
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
 
 from jax import grad
 def sigmoid_function(x):
@@ -11,10 +17,10 @@ class logisticregression:
         self.weights = []
 
     def fit(self,X,y,intercept_addition=True,n_iterations = 100,learning_rate = 0.01,use_jax=False):
+        if(intercept_addition==True):
+            column = np.ones((X.shape[0],1))
+            X = np.hstack((column,X))
         if(self.regularization=="No"):            
-            if(intercept_addition==True):
-                column = np.ones((X.shape[0],1))
-                X = np.hstack((column,X))
             self.weights = np.zeros(X.shape[1])
             for i in range(n_iterations):
                 ### found the predictions for a step 
@@ -28,20 +34,18 @@ class logisticregression:
                 else:
                     gradient_displacement = grad(sigmoid_function)(values)
                     self.weights += learning_rate*gradient_displacement
-        
 
-
-
-       
-        
-        
-        
-        
-        
-        
-        return 0
     
     def predict(self,X):
-        return 0
+        print("prediction started")
+        print(self.weights.shape)
+        print(X.shape)
+        a = np.dot(X.T,self.weights)
+        a[a>=0.5] = 1
+        a[a<0.5] = 0
+        print(a.shape)
+        return a
+
+
 
 
